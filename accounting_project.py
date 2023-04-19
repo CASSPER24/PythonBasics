b9d76366ecc48b05929940259d9ff5939873a427
CASSPER24 <ayesigyederrick@gmail.com>
2023-04-19 21:35:39 +0300

    Intial commit

---
+++ accounting_project.py
@@ -0,0 +1,8 @@
+new_item = input("Do you want to Enter New Item? ").upper()
+if new_item == "YES":
+	item_name = input("Enter Item Name: ")
+	item_list = []
+	item_price = input(f"Enter the Buying price of {item_name}: ")
+
+def function(item_list):
+	pass
---
+++ all_students.py
@@ -0,0 +1,24 @@
+All_Students = [{'Name': 'Frank', 'Reg_No': 'DCS/0023/PW'},
+ {'Name': 'Brenda', 'Reg_No': 'ITS/2900/PD'},
+ {'Name': 'Joy', 'Reg_No': 'ITD/1000/PE'},
+ {'Name': 'EMMA', 'Reg_No': 'YFGUH'}]
+All_Students = [{'Name': 'Frank', 'Reg_No': 'DCS/0023/PW'},
+ {'Name': 'Brenda', 'Reg_No': 'ITS/2900/PD'},
+ {'Name': 'Joy', 'Reg_No': 'ITD/1000/PE'},
+ {'Name': 'Derrick', 'Reg_No': 'wer\\5656\\pr'}]
+All_Students = [{'Name': 'Frank', 'Reg_No': 'DCS/0023/PW'},
+ {'Name': 'Brenda', 'Reg_No': 'ITS/2900/PD'},
+ {'Name': 'Joy', 'Reg_No': 'ITD/1000/PE'},
+ {'Name': 'Joseph', 'Reg_No': 'ITS/7870/PR'}]
+All_Students = [{'Name': 'Frank', 'Reg_No': 'DCS/0023/PW'},
+ {'Name': 'Brenda', 'Reg_No': 'ITS/2900/PD'},
+ {'Name': 'Joy', 'Reg_No': 'ITD/1000/PE'},
+ {'Name': 'Joy', 'Reg_No': 'ITD/6767/PE'}]
+All_Students = [{'Name': 'Frank', 'Reg_No': 'DCS/0023/PW'},
+ {'Name': 'Brenda', 'Reg_No': 'ITS/2900/PD'},
+ {'Name': 'Joy', 'Reg_No': 'ITD/1000/PE'},
+ {'Name': 'Reagan', 'Reg_No': 'ITD/1000/PE'}]
+All_Students = [{'Name': 'Frank', 'Reg_No': 'DCS/0023/PW'},
+ {'Name': 'Brenda', 'Reg_No': 'ITS/2900/PD'},
+ {'Name': 'Joy', 'Reg_No': 'ITD/1000/PE'},
+ {'Name': 'God', 'Reg_No': 'ITS/787/pe'}]
